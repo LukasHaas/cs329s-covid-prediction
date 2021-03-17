eef9dd93b0fd7e0bd8a6db689d04fba8bb431393
Dilara Soylu <21346670+dilarasoylu@users.noreply.github.com>
2021-03-17 11:46:28 -0700

    Update ReturningUser flow

--- src/Pages/ReturningUserPage.py
+++ src/Pages/ReturningUserPage.py
@@ -1,21 +1,29 @@
+import time
 import streamlit as st
+from src.MockData.KeyPhrases import KEY_PHRASES
 
 def app(session_state):
-    st.subheader('Upload Your PCR Result')
-    st.write("""
-    Thank you for coming back. Please enter the unique identifier provided
-    to you when you provided your cough samples.
-    """)
-    identifier = st.text_input('Type your identifier and hit Enter:')
+  st.subheader('Upload Your PCR Result')
+  st.write("""
+  Thank you for coming back. Please enter the unique phrase you have chosen
+  when you provided your cough samples.
+  """)
 
-    # TODO check if valid identifier
-    if identifier:
-        test_date = st.date_input("Enter the date of the PCR Test")
-        st.selectbox('What was your PCR test result?', ['Negative', 'Positive'])
-        upload_button = st.button('Upload PCR Test Result')
-        if upload_button:
-            try:
-              # TODO Upload action
-              st.success('Successfully uploaded PCR result!')
-            except:
-              st.error('An error occured while uploading the PCR result. Please try again.')
+  phrase = st.text_input('Type your phrase')
+  test_date = st.date_input("Enter the date of the PCR Test")
+  st.selectbox('What was your PCR test result?', ['Negative', 'Positive'])
+  upload_button = st.button('Upload PCR Test Result')
+  if upload_button:
+    if phrase.strip() not in KEY_PHRASES:
+        st.error("""
+        We couldn't find a cough sample linked to the provided phrase. Are you
+        sure you typed your phrase correctly?
+        """)
+    else:
+      with st.spinner('Uploading the PCR test results ...'):
+        try:
+          time.sleep(2) # TODO replace with actual upload
+          st.success('Successfully uploaded PCR result!')
+        except:
+          st.error('An error occured while uploading the PCR result. Please try again.')
+          print(e)
